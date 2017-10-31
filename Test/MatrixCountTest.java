5b4ef270caf41bb3c2f061002e3e092603112973
kavall <kavall1998@gmail.com>
2017-10-31 18:00:18 +0300

    Fix

--- Test/MatrixCountTest.java
+++ Test/MatrixCountTest.java
@@ -8,9 +8,8 @@ import static org.junit.Assert.*;
 public class MatrixCountTest {
     @Test
     public void countWay() throws Exception {
-        int[][] arr = {{2,1,1,2},{3,2,1,44},{3,1,1,0}};
-        int[][] arr1 = {{3,2,1,3,1,1,3,1},{1,1,2,1,1,1,1,1},{2,1,1,1,1,1,1,1},{3,1,2,4,1,1,1,2},{1,1,1,1,1,1,1,1},{1,1,1,4,1,1,1,0}};
-        MatrixCount m = new MatrixCount(arr1);
-        assertEquals(5,m.countWay(0,0));
+        int[][] arr = {{1,1,1},{1,1,1},{1,1,1}};
+        MatrixCount m = new MatrixCount(arr);
+        assertEquals(6,m.countWay(0,0));
     }
 }
--- src/MatrixCount.java
+++ src/MatrixCount.java
@@ -1,21 +1,47 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Created by kaval on 02.10.2017.
  */
 public class MatrixCount {
-    static int [][] arr ;
+    static int[][] arr;
+    static int[][] copyArr = new int[3][3];
     static int count;
-    public MatrixCount (int arr[][]){
+
+    public MatrixCount(int arr[][]) {
         this.arr = arr;
     }
-    public int countWay(int i , int j){
-        if (j < arr[0].length-1 || i<arr.length-1){
-            if (i + arr[i][j]<arr.length)
-                countWay(i+arr[i][j],j);
-            if (j+arr[i][j]<arr[0].length)
-                countWay(i,j+arr[i][j]);
-        }
-        else if (i==arr.length-1 || j==arr[0].length-1)
-            count++;
+
+    public int countWay(int i, int j) {
+            if (j < arr[0] .length - 1 || i < arr.length - 1) {
+                if (i + arr[i][j] < arr.length) {
+                    if (arr[i][j]>0) {
+                        copyArr[i][j] = arr[i][j];
+                        countWay(i + arr[i][j], j);
+                    }
+                }
+                if (j + arr[i][j] < arr[0].length) {
+                    if (arr[i][j]>0) {
+                    copyArr[i][j] = arr[i][j];
+                    countWay(i, j + arr[i][j]);
+                    }
+                }
+            } else if (i == arr.length - 1 || j == arr[0].length - 1) {
+                copyArr[i][j] = arr[i][j];
+                int countI = arr.length;
+                int countJ = arr[0].length;
+                copyArr[0][0] = arr[0][0];
+                for (int k = 0; k < countI; k++) {
+                    for (int v = 0; v < countJ; v++) {
+                        System.out.print(copyArr[k][v]);
+                    }
+                    System.out.println(" ");
+                }
+                System.out.println(" ");
+                copyArr = new int[4][3];
+                count++;
+            }
         return count;
     }
 }
